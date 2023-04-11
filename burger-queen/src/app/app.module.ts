e492c259d5cb1a4764afaadb697df0dd0ee5eefb
Ale Santos Coz <ale.santoscoz@gmail.com>
2023-04-11 08:38:51 -0500

    appRoutes login

--- burger-queen/src/app/app.module.ts
+++ burger-queen/src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const appRoutes:Routes=[
+{path:'',component:LoginComponent} //crear objeto por cada ruta
+//el path es la ruta que saldrá en el navegador y component el componente de la app
+]
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,8 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
