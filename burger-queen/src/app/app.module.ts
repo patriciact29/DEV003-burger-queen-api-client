7080faa8a802f37bb352e56662cbe2f8e5c0f33e
Ale Santos Coz <ale.santoscoz@gmail.com>
2023-04-11 09:35:54 -0500

    home component y estilos login

--- burger-queen/src/app/app.module.ts
+++ burger-queen/src/app/app.module.ts
@@ -4,18 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
 const appRoutes:Routes=[
-{path:'',component:LoginComponent} //crear objeto por cada ruta
-//el path es la ruta que saldrá en el navegador y component el componente de la app
+{path:'',component:LoginComponent},//crear objeto por cada ruta
+{path:'home',component:HomeComponent}
 ]
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
