20fdf3b78aa5cb1dce7562607fc2a9d38780d8cd
Ale Santos Coz <ale.santoscoz@gmail.com>
2023-05-08 22:31:30 -0500

    imprimiendo nombres de productos

--- burger-queen/src/app/home/order-list/order-list.component.ts
+++ burger-queen/src/app/home/order-list/order-list.component.ts
@@ -13,9 +13,11 @@ export class OrderListComponent implements OnInit {
   constructor(private orderServ:OrderService) { }
 
    ngOnInit(): void {
+    //se llama a la función del service
     this.orderServ.getOrders()
     .subscribe(res =>{
       console.log(res)
+      //igualamos data order al array de respuesta
     this.dataOrder= res
     })
 }
